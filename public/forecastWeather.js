10bc208b861bca3475f81c08f9ed292292315e79
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-02-09 01:03:07 +0800

    moved 7 day forecast to separate file

---
+++ public/forecastWeather.js
@@ -0,0 +1,39 @@
+//html elements
+let th = document.querySelectorAll("table tr .day");
+let dayTemps = document.querySelectorAll("table .tableInfo .dayTemps");
+let nightTemps = document.querySelectorAll("table .tableInfo .nightTemps");
+let feelsLike = document.querySelectorAll("table .tableInfo .feelsLike");
+
+async function getForcast(forecast) {
+  try {
+    const response = await fetch(forecast);
+    const data = await response.json();
+    const days = data.daily.map(day => {
+      let dayLong = new Date(day.dt * 1000).toLocaleString('en-us', { weekday: 'long' });
+      let dayShorted = dayLong.substr(0, 3);
+      return {
+        dayShorted: dayShorted,
+        maxTemp: day.temp.max,
+        minTemp: day.temp.min,
+        nightTemp: day.temp.night,
+        feelsLike: day.feels_like.day
+      };
+    })
+    th.forEach((header, index) => {
+      header.textContent = days[index].dayShorted;
+    });
+    dayTemps.forEach((row, index) => {
+      row.textContent = `${(days[index].minTemp - 273.15).toFixed(1)} - ${(days[index].maxTemp - 273.15).toFixed(1)}`;
+    });
+
+    nightTemps.forEach((row, index) => {
+      row.textContent = `${(days[index].nightTemp - 273.15).toFixed(1)}`;
+    });
+
+    feelsLike.forEach((row, index) => {
+      row.textContent = `${(days[index].feelsLike - 273.15).toFixed(1)}`;
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
