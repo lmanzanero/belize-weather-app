1cd8b72c4334dcca6618a2a437c5fd31fa295d4f
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-02-16 00:59:25 +0800

    removed old seven day forecast logic

--- forecastWeather.js
+++ forecastWeather.js
@@ -1,8 +1,3 @@
-//html elements
-let th = document.querySelectorAll("table tr .day");
-let dayTemps = document.querySelectorAll("table .tableInfo .dayTemps");
-let nightTemps = document.querySelectorAll("table .tableInfo .nightTemps");
-let feelsLike = document.querySelectorAll("table .tableInfo .feelsLike");
 let sunrise = document.querySelector(".sunrise");
 let sunset = document.querySelector(".sunset");
 let moonrise = document.querySelector(".moonrise");
@@ -196,34 +191,8 @@ async function getForcast(forecast) {
       <p>
       ${tide.tide_date_type}: <span>${tide.tide_display}</span>
       </p>`);
-    })
-
-    //weekly/days
-    const days = weekly.daily.map(day => {
-      let dayLong = new Date(day.dt * 1000).toLocaleString('en-us', { weekday: 'long' });
-      let dayShorted = dayLong.substr(0, 3);
-      return {
-        dayShorted: dayShorted,
-        maxTemp: day.temp.max,
-        minTemp: day.temp.min,
-        nightTemp: day.temp.night,
-        feelsLike: day.feels_like.day
-      };
-    })
-    th.forEach((header, index) => {
-      header.textContent = days[index].dayShorted;
     });
-    dayTemps.forEach((row, index) => {
-      row.textContent = `${(days[index].minTemp - 273.15).toFixed(1)} - ${(days[index].maxTemp - 273.15).toFixed(1)}`;
-    });
-
-    nightTemps.forEach((row, index) => {
-      row.textContent = `${(days[index].nightTemp - 273.15).toFixed(1)}`;
-    });
-
-    feelsLike.forEach((row, index) => {
-      row.textContent = `${(days[index].feelsLike - 273.15).toFixed(1)}`;
-    })
+    
   } catch (error) {
     console.log(error);
   }
